3fad590330f4ad65cccb62b3c6a4451cb5dc1ada
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-03-21 11:07:27 -0300

    21/03/2023

---
+++ projeto_conta/Conta.cs
@@ -0,0 +1,22 @@
+﻿namespace projeto_conta;
+class Conta
+{
+    public int Numero{get; set;} 
+    private double Saldo {get; set;}
+    public double Limite {get; private set;}
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+
+    public void Depositar(double valor){
+        this.Saldo += valor;
+    }
+    public void Sacar(double valor){
+        this.Saldo -= valor;
+    }
+
+    public double mostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+}
---
+++ projeto_conta/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("PROJETO BANCO ETEC MCM!");
+        Conta c = new Conta();
+        //c.Saldo = 1000;
+        c.Depositar(1000);
+        c.Sacar(800);
+        //c.Limite = 500;
+        c.AjustarLimite(500);
+
+        double valorTotal = c.mostrarSaldo();
+
+        //Console.WriteLine("Seu saldo é de: "+ c.Saldo + " Com limite de: " + c.Limite);
+
+        Console.WriteLine("Seu saldo total é de " + valorTotal);
+        //Console.WriteLine("Seu saldo total é de " + c.Saldo);
+    }
+}
