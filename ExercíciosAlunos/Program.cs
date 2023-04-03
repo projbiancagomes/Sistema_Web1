badafc1e916d942eccf5c78792d66158e62e2863
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-04-03 19:39:59 -0300

    14/03/2023

---
+++ ExercíciosAlunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Outro Aluno";
+        aluno1.nota1 = 4;
+        aluno1.nota2 = 5;
+
+        aluno1.mensagem();
+    }
+}
