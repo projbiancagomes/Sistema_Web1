e9289c2cfc3fc9038d546efad842b6958b5b8325
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-04-04 09:31:53 -0300

    28/03/2023

---
+++ Método_construtor/projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+         Pessoa p1 = new Pessoa("FULANO",23);
+         //p1.InsereNome("BIANCA");
+         //p1.InsereSobreNome("GOMES");
+         //p1.InsereIdade(16);
+
+         p1.MostrarDados();
+    }
+}
