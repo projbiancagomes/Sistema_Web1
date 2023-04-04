90587952e210f6dde6f832d039c60e3145bdf83e
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-04-04 09:48:59 -0300

    28/02/2023

---
+++ Aula_28_02_23/Aviao/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex_aviao;
+
+class Program{
+    static void Main(string[] args){
+        
+        Console.WriteLine("Exercicio Aviao POO");
+        Aviao aviao01 = new Aviao();
+
+        aviao01.modelo = "Airbus A330";
+        aviao01.marca = "TAM";
+        
+        aviao01.exibir();
+        aviao01.acelerar();
+        aviao01.exibir();
+        aviao01.acelerar();
+        aviao01.exibir();
+        
+    }
+}
