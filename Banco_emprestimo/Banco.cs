57e2d5fa722bdc1765ed2f811ccb3bac0c6c3bf9
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-04-25 10:43:53 -0300

    18/04/2023

---
+++ Banco_emprestimo/Banco.cs
@@ -0,0 +1,45 @@
+﻿namespace Banco_emprestimo;
+class Banco
+{
+   public double valor {get;set;}
+   public double prazo {get;set;}
+   public double taxa {get;set;}
+   public double mes {get;set;}
+   public double total {get;set;}
+
+
+   public string emprestimo(double n1, double n2){
+    this.valor = n1;
+    this.prazo = n2;
+    double emprestimo = this.valor / this.prazo;
+    string result1 = " Valor da prestação do seu empréstimo: R$" +emprestimo;
+    return result1;
+   }
+
+   public string juros(double n1,double n3){
+   this.valor = n1;
+   this.taxa = n3;
+   double juros = this.valor * this.taxa;
+   string result2 = " Valor do juros do seu empréstimo: " +juros+"%";
+   return result2;
+   }
+
+   public string prestacao (double n3, double n4){
+    this.taxa = n3;
+    this.mes = n4;
+    double prestacao = this.taxa + this.mes;
+    string result3 = " Valor do juros com o valor da prestação de seu empréstimo: R$"+prestacao;
+    return result3;
+   }
+   public string ano (double n2, double n4){
+      this.prazo = n2;
+      this.mes = n4;
+      double ano = this.prazo * this.mes;
+      string result4 = " Valor total a pagar: R$"+ano;
+      return result4;
+   }
+   
+   public void imprime(string texto){
+    Console.WriteLine(texto);
+   }
+}
---
+++ Banco_emprestimo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Banco_emprestimo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Banco b = new Banco();
+
+        b.imprime(b.emprestimo(1000,12));
+        b.imprime(b.juros(1000,0.06));
+        b.imprime(b.prestacao(60,83));
+         b.imprime(b.ano(12,143.33));
+    }
+}
